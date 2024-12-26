a768145ddff3c78e636f448f596fe4bd0dd211e3
lais <mosukemono@gmail.com>
2024-12-26 19:12:17 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor, fullWidth}) => {
+    return (
+      <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${backgroundColor?`${backgroundColor} ${textColor} ${borderColor}`: ` bg-coral-red  text-white`} rounded-full hover:bg-[#ff9c6e] ${fullWidth && 'w-full' }`}>{label}
+      {iconUrl &&<img src={iconUrl} alt="arrow right icon" className="ml-2 roundend-full w-5 h-5 " />}
+      </button>
+    )
+  }
+  
+  export default Button
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+import Services from './Services';
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReview,
+    Subscribe,
+    Footer
+}
